f74b1fa5ac2f0eeddbbdb9286e81cdfa7bd318fd
Equi3327 <vittal3327@gmail.com>
2026-05-25 11:09:19 +0530

    ci: add automated next.js deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  /// TODO: Remove these once i have my domain ready and can set up proper hosting with a custom server
+  basePath: '/velvenest', 
+  assetPrefix: '/velvenest',
 };
 
 export default nextConfig;
